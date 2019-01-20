c5a6dc10ac9a894e47020c8e4c644353ac8f5ade
Kamil Wcislo <kamil.wcislo@lpnplant.io>
2019-01-20 12:39:00 +0100

    include/image.mk: enable multi-cpu squashfs image creation
    Signed-off-by: Kamil Wcislo <kamil.wcislo@lpnplant.io>

--- include/image.mk
+++ include/image.mk
@@ -199,11 +199,13 @@ endef
 $(eval $(foreach S,$(JFFS2_BLOCKSIZE),$(call Image/mkfs/jffs2/template,$(S))))
 $(eval $(foreach S,$(NAND_BLOCKSIZE),$(call Image/mkfs/jffs2-nand/template,$(S))))
 
+NPROCS:=$(shell grep -c ^processor /proc/cpuinfo)
+
 define Image/mkfs/squashfs
 	$(STAGING_DIR_HOST)/bin/mksquashfs4 $(call mkfs_target_dir,$(1)) $@ \
 		-nopad -noappend -root-owned \
 		-comp $(SQUASHFSCOMP) $(SQUASHFSOPT) \
-		-processors 1 \
+		-processors $(NPROCS) \
 		$(if $(SOURCE_DATE_EPOCH),-fixed-time $(SOURCE_DATE_EPOCH))
 endef
 
